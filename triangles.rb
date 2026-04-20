6331a8ecb70336853096c6483dede2138a9c210e
Gaurav <gauravjeena@outlook.com>
2026-04-20 19:51:14 +0200

    Additional challenges

--- triangles.rb
+++ triangles.rb
@@ -1,15 +1,27 @@
-def is_a_triangle?(sides)
-  if sides.any? { |side| side.zero? }
+class Triangle
+  attr_reader :sides
 
+  def initialize(*sides)
+    @sides = sides
+    raise ArgumentError, "Invalid triangle" unless is_a_triangle?
+    raise ArgumentError, "Expected 3 sides, given more" if sides.count > 3
+  end
+
+  def is_a_triangle?
+    return false if sides.any? { |side| side < 0 }
+    sides.each do |side|
+      return false if (sides.sum - side) <= side
+    end
+    true
+  end
+
+  def type
+    if sides.all? { |side| sides[0] == side }
+      "equilateral" 
+    elsif sides.any? { |side| sides.count(side) == 2 }
+      "isosceles"
+    else
+      "scalene"
+    end
+  end
 end
-
-def which_triangle(*sides_array)
-  if sides_array.any? { |side| side.zero? } && 
-end
-
-
-p which_triangle(3,5,6)
-p which_triangle(4,4,4)
-p which_triangle(0,6,7)
-p which_triangle(4,4,6)
-p which_triangle(1,2,1)
